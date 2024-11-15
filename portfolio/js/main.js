fe9422a52ac8309932cef85f5b9177bff7500f0b
shotikobeboshvili <156705895+shotikobeboshvili@users.noreply.github.com>
2024-11-15 18:37:01 +0400

    Add files via upload

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,15 +1,7 @@
-const navMenu = document.getElementById('nav_menu'),
-      navToggle = document.getElementById('toggle'),
-      navClose = document.getElementById('nav_close');
+const toggleButton = document.getElementById('toggle');
+const navMenu = document.getElementById('nav_menu');
 
-      if(navToggle) {
-        navToggle.addEventListener('click', () =>{
-            navMenu.classList.add('show-menu')
-        })
-      }
-
-      if(navClose){
-        navClose.addEventListener('click', () => {
-            navMenu.classList.remove('show-menu')
-        })
-    };
+// ღილაკზე click მოვლენაზე მოსმენა
+toggleButton.addEventListener('click', () => {
+  navMenu.classList.toggle('show-menu'); // კლასი "open"-ის დამატება ან მოხსნა
+});
